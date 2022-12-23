8fbf5cd39a5c3ce06b962e69aa8dd08988207be2
Ramirisu <labyrinth.ramirisu@gmail.com>
2022-12-23 18:40:43 +0800

    Fix copyright header.

--- test/handlers_invoker_test.cpp
+++ test/handlers_invoker_test.cpp
@@ -1,9 +1,8 @@
 //
 // Copyright (c) 2022 Ramirisu (labyrinth dot ramirisu at gmail dot com)
 //
-// Distributed under the ramirisu Software License, Version 1.0. (See
-// accompanying file LICENSE_1_0.txt or copy at
-// http://www.ramirisu.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include <fitoria_test.h>
