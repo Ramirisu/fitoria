f5133ef2edc1caf93987258f0086bf116d42d107
Ramirisu <labyrinth.ramirisu@gmail.com>
2023-01-20 19:02:26 +0800

    Add graceful shutdown example.

--- README.md
+++ README.md
@@ -395,6 +395,35 @@ int main()
 }
 
 ```
+#### Graceful Shutdown
+
+Use `net::signal_set` to handle signals to shutdown the server gracefully. ([Graceful Shutdown Example](https://github.com/Ramirisu/fitoria/blob/main/example/web/graceful_shutdown.cpp))
+
+```cpp
+
+int main()
+{
+  auto server = http_server::builder().build();
+  server.bind("127.0.0.1", 8080);
+
+  // Create the execution context and spawn coroutines on it
+  net::io_context ioc;
+  net::co_spawn(ioc, server.async_run(), net::detached);
+
+  // Register signals to stop the execution context
+  net::signal_set signal(ioc, SIGINT, SIGTERM);
+  signal.async_wait([&](auto, auto) { ioc.stop(); });
+
+  // Run the execution context with thread pool
+  const std::uint32_t num_threads = std::thread::hardware_concurrency();
+  net::thread_pool tp(num_threads);
+  for (auto i = std::uint32_t(1); i < num_threads; ++i) {
+    net::post(tp, [&]() { ioc.run(); });
+  }
+  ioc.run();
+}
+
+```
 
 ### HTTP Client
 
---
+++ example/web/graceful_shutdown.cpp
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2022-2023 Ramirisu (labyrinth dot ramirisu at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <fitoria/fitoria.hpp>
+
+using namespace fitoria;
+
+int main()
+{
+  auto server = http_server::builder().build();
+  server.bind("127.0.0.1", 8080);
+
+  // Create the execution context and spawn coroutines on it
+  net::io_context ioc;
+  net::co_spawn(ioc, server.async_run(), net::detached);
+
+  // Register signals to stop the execution context
+  net::signal_set signal(ioc, SIGINT, SIGTERM);
+  signal.async_wait([&](auto, auto) { ioc.stop(); });
+
+  // Run the execution context with thread pool
+  const std::uint32_t num_threads = std::thread::hardware_concurrency();
+  net::thread_pool tp(num_threads);
+  for (auto i = std::uint32_t(1); i < num_threads; ++i) {
+    net::post(tp, [&]() { ioc.run(); });
+  }
+  ioc.run();
+}
