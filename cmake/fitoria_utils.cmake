f2a8025a60578aa4d85b2e7ebcdfd12ed8586d7d
Ramirisu <labyrinth.ramirisu@gmail.com>
2023-01-30 15:21:28 +0800

    Format cmake files.

--- cmake/fitoria_utils.cmake
+++ cmake/fitoria_utils.cmake
@@ -1,14 +1,17 @@
 function(fitoria_target_compile_option target_name)
   target_link_libraries(${target_name} PRIVATE fitoria)
+
   if(WIN32)
     target_compile_definitions(${target_name} PRIVATE _WIN32_WINNT=0x0A00)
   endif()
+
   if((CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-     OR (CMAKE_CXX_COMPILER_FRONTEND_VARIANT MATCHES "MSVC"))
+    OR(CMAKE_CXX_COMPILER_FRONTEND_VARIANT MATCHES "MSVC"))
     target_compile_options(${target_name} PRIVATE /utf-8 /EHsc /bigobj /W4 /WX)
   else()
     target_compile_options(${target_name} PRIVATE -Wall -Wextra -Werror
-                                                  -pedantic -pedantic-errors)
+      -pedantic -pedantic-errors)
+
     if(FITORIA_ENABLE_CODECOV)
       target_compile_options(${target_name} PRIVATE --coverage)
       target_link_libraries(${target_name} PRIVATE gcov)
