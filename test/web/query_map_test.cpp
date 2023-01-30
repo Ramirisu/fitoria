4aa3027b86a140158b06b4074c5c67c2bc5557c1
Ramirisu <labyrinth.ramirisu@gmail.com>
2023-01-30 15:39:53 +0800

    Add tests for `query_map`.

---
+++ test/web/query_map_test.cpp
@@ -0,0 +1,55 @@
+//
+// Copyright (c) 2022-2023 Ramirisu (labyrinth dot ramirisu at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <fitoria_test.h>
+
+#include <fitoria/web/query_map.hpp>
+
+using namespace fitoria;
+
+TEST_SUITE_BEGIN("web.query_map");
+
+TEST_CASE("basic")
+{
+  auto m = query_map();
+  const auto& c = m;
+
+  CHECK(m.empty());
+  CHECK_EQ(m.size(), 0);
+  CHECK(!m.contains("name1"));
+  CHECK_EQ(m.get("name1"), nullopt);
+  CHECK_EQ(c.get("name1"), nullopt);
+
+  m.set("name1", "value");
+  CHECK(!m.empty());
+  CHECK_EQ(m.size(), 1);
+  CHECK(m.contains("name1"));
+  CHECK(!m.contains("name2"));
+  CHECK_EQ(m.get("name1"), "value");
+  CHECK_EQ(c.get("name1"), "value");
+
+  m.set("name2", "value2");
+  CHECK(!m.empty());
+  CHECK_EQ(m.size(), 2);
+  CHECK(m.contains("name1"));
+  CHECK(m.contains("name2"));
+  CHECK_EQ(m.get("name2"), "value2");
+  CHECK_EQ(c.get("name2"), "value2");
+
+  CHECK(!m.erase("name3"));
+
+  CHECK(m.erase("name1"));
+  CHECK(!m.empty());
+  CHECK_EQ(m.size(), 1);
+  CHECK_EQ(m.get("name1"), nullopt);
+  CHECK_EQ(c.get("name1"), nullopt);
+
+  m.clear();
+  CHECK(m.empty());
+}
+
+TEST_SUITE_END();
