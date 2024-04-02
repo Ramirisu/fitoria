926ba6be8cbde34bd0aabe97fa5596a6076976fc
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-04-03 05:28:51 +0800

    Add thread pool example.

---
+++ example/web/thread_pool.cpp
@@ -0,0 +1,34 @@
+//
+// Copyright (c) 2024 Ramirisu (labyrinth.ramirisu@gmail.com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <fitoria/web.hpp>
+
+using namespace fitoria;
+using namespace fitoria::web;
+
+int main()
+{
+  auto ioc = net::io_context();
+  auto server
+      = http_server_builder(ioc)
+            .serve(route::get<"/">(
+                [](std::string body) -> net::awaitable<http_response> {
+                  co_return http_response(http::status::ok)
+                      .set_field(http::field::content_type,
+                                 http::fields::content_type::plaintext())
+                      .set_body(body);
+                }))
+            .build();
+  server.bind("127.0.0.1", 8080);
+
+  const std::size_t threads = std::thread::hardware_concurrency();
+  auto tp = net::static_thread_pool(threads);
+  for (std::size_t i = 0; i < threads; ++i) {
+    net::post(ioc, [&]() { ioc.run(); });
+  }
+  ioc.run();
+}
