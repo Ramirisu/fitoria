c0ef499a0a571020c8d0f943cb562e5e4070bfa7
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-04-07 21:02:02 +0800

    More test for async streams.

--- test/web/test_web_async_readable_stream.cpp
+++ test/web/test_web_async_readable_stream.cpp
@@ -14,8 +14,10 @@
 #include <fitoria/test/http_server_utils.hpp>
 
 #include <fitoria/web/async_read_until_eof.hpp>
+#include <fitoria/web/async_readable_eof_stream.hpp>
 #include <fitoria/web/async_readable_file_stream.hpp>
 #include <fitoria/web/async_readable_stream_concept.hpp>
+#include <fitoria/web/async_readable_vector_stream.hpp>
 
 #include <fstream>
 
@@ -24,6 +26,51 @@ using namespace fitoria::web;
 using namespace fitoria::test;
 
 TEST_SUITE_BEGIN("[fitoria.web.async_readable_stream]");
+
+TEST_CASE("async_readable_eof_stream")
+{
+  sync_wait([&]() -> awaitable<void> {
+    auto stream = async_readable_eof_stream();
+    auto buffer = std::array<std::byte, 4096>();
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))).error(),
+             make_error_code(net::error::eof));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))).error(),
+             make_error_code(net::error::eof));
+  });
+}
+
+TEST_CASE("async_readable_vector_stream: empty")
+{
+  sync_wait([&]() -> awaitable<void> {
+    auto stream = async_readable_vector_stream(
+        std::vector<std::byte>(9, std::byte(0x40)));
+    auto buffer = std::array<std::byte, 4>();
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))),
+             std::size_t(4));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))),
+             std::size_t(4));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))),
+             std::size_t(1));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))).error(),
+             make_error_code(net::error::eof));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))).error(),
+             make_error_code(net::error::eof));
+  });
+}
+
+TEST_CASE("async_readable_vector_stream")
+{
+  sync_wait([&]() -> awaitable<void> {
+    auto stream = async_readable_vector_stream();
+    auto buffer = std::array<std::byte, 4096>();
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))),
+             std::size_t(0));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))).error(),
+             make_error_code(net::error::eof));
+    CHECK_EQ((co_await stream.async_read_some(net::buffer(buffer))).error(),
+             make_error_code(net::error::eof));
+  });
+}
 
 #if defined(BOOST_ASIO_HAS_FILE)
 
@@ -42,7 +89,7 @@ TEST_CASE("async_readable_file_stream")
                              "test_web_async_readable_file_stream.txt",
                              net::file_base::read_only))),
         data);
-  }());
+  });
 }
 
 #endif
