485bd42f777700b84da69c3abc8aec7810b97d5d
Ramirisu <labyrinth.ramirisu@gmail.com>
2024-06-21 18:19:31 +0800

    Update thread pool example.

--- example/web/basic/thread_pool.cpp
+++ example/web/basic/thread_pool.cpp
@@ -12,21 +12,17 @@ using namespace fitoria::web;
 
 int main()
 {
-  auto ioc = net::io_context();
+  auto tp = net::static_thread_pool();
   auto server
-      = http_server::builder(ioc)
-            .serve(route::get<"/">([](std::string body) -> awaitable<response> {
-              co_return response::ok()
-                  .set_header(http::field::content_type, mime::text_plain())
-                  .set_body(body);
-            }))
+      = http_server::builder(tp.get_executor())
+            .serve(
+                route::post<"/">([](std::string body) -> awaitable<response> {
+                  co_return response::ok()
+                      .set_header(http::field::content_type, mime::text_plain())
+                      .set_body(body);
+                }))
             .build();
   server.bind("127.0.0.1", 8080);
 
-  const std::size_t threads = std::thread::hardware_concurrency();
-  auto tp = net::static_thread_pool(threads);
-  for (std::size_t i = 0; i < threads; ++i) {
-    net::post(ioc, [&]() { ioc.run(); });
-  }
-  ioc.run();
+  tp.join();
 }
